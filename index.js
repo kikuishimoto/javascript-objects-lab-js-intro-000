589da4bcadd2fc66e5de9e759f2e8dc47bb79a84
kiku ishimoto <kikuish@gmail.com>
2020-04-12 02:18:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   obj[key] = value
-  return obj
+  return recipes
 }
